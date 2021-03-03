46c65573c9b8c13a0c2933e1838160c0852f0db4
sj3603 <56056374+sj3603@users.noreply.github.com>
2021-03-03 11:38:03 -0500

    started work on map generation
    started working on array map generation:
    - allocated #s to different entities
    - created 15 x 10 map array
    - started work on loop to populate array

--- src/data.js
+++ src/data.js
@@ -1,1 +1,57 @@
 // Holds all of the arrays and constant data needed for the game to run
+// -- Level Key --
+// 0 = Nothing
+// 1 = Player Start
+// 2 = Wumpus
+// 3 = Treasure
+// 4 = Pit
+// 5 = Bat
+
+let playerSpawnCount = 0;
+let playerSpawnMax = 1;
+
+let wumpusSpawnCount = 0;
+let wumpusSpawnMax = 1;
+
+let treasureSpawnCount = 0;
+let treasureSpawnMax = 1;
+
+let pitSpawnCount = 0;
+let pitSpawnMax = 0;
+
+let batSpawnCount = 0;
+let batSpawnMax = 0;
+
+let totalEntityCount = playerSpawnMax + wumpusSpawnMax + treasureSpawnMax + pitSpawnMax + batSpawnMax; 
+
+let mapX = 15;
+let mapY = 10;
+
+let map = [
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+];
+
+function MapGen(){
+    for (let i = 0; i < totalEntityCount; i++)
+    {
+        randMapX = Math.random(0,mapX);
+        randMapY = Math.random(0,mapY);
+        if(map[randMapX,randMapY] != 0)
+        {
+            i--;
+        }
+        else
+        {
+            
+        }
+    }
+};
